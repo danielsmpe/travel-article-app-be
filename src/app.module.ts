d02698a919d01d91693bb4c9618b9cad5cfa076a
Daniel Sampe <danielsampe61@gmail.com>
2025-08-01 21:16:32 +0800

    feat: setup auth and user modules with registration feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'POLQWS',
+      database: 'travel_app',
+      autoLoadEntities: true,
+      synchronize: true, // development
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Users } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(Users) private userRepo: Repository<Users>) {}
+
+  async register(
+    createUserDto: CreateUserDto,
+  ): Promise<Omit<Users, 'password'>> {
+    const { username, password } = createUserDto;
+
+    const existingUser = await this.userRepo.findOne({ where: { username } });
+    if (existingUser) {
+      throw new ConflictException('Username already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = this.userRepo.create({
+      username,
+      password: hashedPassword,
+    });
+    const savedUser = await this.userRepo.save(newUser);
+
+    const { password: _, ...userWithoutPassword } = savedUser;
+    return userWithoutPassword;
+  }
+}
