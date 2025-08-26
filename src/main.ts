eb1d32b48bd86cd7b227b83ea07e0283843cd801
Daniel Sampe <danielsampe61@gmail.com>
2025-08-26 15:54:03 +0800

    update: add new FE url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:4173'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:4173',
+      'https://travel-article-iota.vercel.app/',
+    ],
     credentials: true,
   });
 
